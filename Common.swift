332b292bff878daf7a61b49a70fd9e3a028ebc2b
Yuneeee <doyune0707@gmail.com>
2019-07-09 11:09:55 +0900

    key

---
+++ Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  DoyunWeather
+//
+//  Created by Doyun on 09/07/2019.
+//  Copyright © 2019 Doyun. All rights reserved.
+//
+
+import Foundation
+
+let appKey = "31f396d1-e715-4e5f-a57a-7d569c2c03de"
